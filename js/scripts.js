1d8a756cf20b8a815a3e32d1b258a9eba5dce01e
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-03 11:51:35 -0500

    simple start button wworks. additional functionality may be needed when rest of game is built.

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,12 @@
 //when roll button is clicked, store the above value in a p1 variable, p2's pick in a p2 variable, then compare with roll result. tally win/loss.
 
 //outside onload
+var startGame = function () {
+  game.score.player1Wins=0;
+  game.score.player2Wins=0;
+  //loop for n players? keys.game.score.'player'+i+'Wins = 0;
+}
+
 var game = {
   rounds: 3,
   player1: 'Player 1',
@@ -17,12 +23,15 @@ var game = {
     player1Wins: 0,
     player2Wins: 0,
   },
-  startGame: function () {
+  startGame : function () {
     game.score.player1Wins=0;
     game.score.player2Wins=0;
     //loop for n players? keys.game.score.'player'+i+'Wins = 0;
-  }
+    console.log('scores set to 0');
+  },
 }
+
+
   console.log(game);
   console.log('that was game obj');
 
@@ -32,8 +41,12 @@ $(function(){
   //onload functions?
 
 //event listener for start button
-var startListener=$('button')[0];
-startListener.on('click', game.startGame())
+//$('button').eq(0) returns a jquery object with 1 element. use this to make listeners. below may be wonky, improve.
+$('button').eq(0).on('click', function(){ game.startGame();
+
+   });
+// $('button').eq(0).onclick= /*game.startGame*/ console.log('$(\'button\').eq(0).onclick= worked');
+// startListener
 
 //on click, start game
 
