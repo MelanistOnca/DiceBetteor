28d288979917ad76b55147c81cf4a1ee0283568d
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-03 22:10:26 -0500

    MVP almost done. need to add score display reset to startGame()

--- js/scripts.js
+++ js/scripts.js
@@ -42,7 +42,10 @@ function Dice(){
 //EVENT LISTENERS START HERE
 $('button').eq(0).on('click', game.startG);
 $('button').eq(1).on('click', resultFieldUpdate);
-//$('button').eq(1).on('click', roundWin);
+$('button').eq(1).on('click', roundScore);
+
+
+
 
 console.log('document loaded');
 });
@@ -60,22 +63,60 @@ console.log('document loaded');
   //on roll, compare result to pNChooser, modify scores
   // $('button').eq(0).on('click',)
 function roundScore (){
-//stuff
+//check for wins, increment scores accordingly.
 
-  if($('.p1Chooser')[0].value===parseInt($('.resultField').text() ) ){
+  if( $('.currentScore1').text() !== 'SCORE'){
+    // console.log('.currentScore1 is not equal to SCORE');
+  } else{
+
+    $('.currentScore1').text('0');
+  }
+  //what i think i'm doing with these functions is setting the currentScore fiels to 0. almost certainly a better way to do this. In fact, after just typing this, I should initialize them to 0 in the html. but now i want to see if my logic works, so i'll do that later.
+  if( $('.currentScore2').text() !== 'SCORE'){
+    // console.log('.currentScore2 is not equal to SCORE');
+  } else{
+    $('.currentScore2').text('0');
+  }
+
+  if($('.p1Chooser')[0].value!==$('.resultField').text() ){//the condition here pops an error on load.
+
+    console.log(game.score);
+    console.log('p1 did not match. no points earned');
+  } else if($('.p1Chooser')[0].value===$('.resultField').text()){
     game.score.player1Wins++;
-    if( $('.currentScore2').text('SCORE') ){
-      console.log('test SCORE check')
-    } else {
-      $('.currentScore2').text('' +0); //THIS SHOULD BE INCREMENT, THIS IS FOR TESTING, IS BAD, AND YOU SHOULD FEEL BAD
-    }
+    console.log(game.score);
+    $('.currentScore1').text(game.score.player1Wins);
+  } else {
+    console.log('you are in the player1 score check land of the dead, where the living should not be.');
   }
-  if($('.p2Chooser')[0].value===parseInt($('.resultField').text() ) ){
-    game.score.player2Wins++;
-  }
+//using if (!not thing){nothing} else if (thing){do stuff} else{the world is broken}
 
+  if($('.p2Chooser')[0].value===$('.resultField').text()  ){
+
+    game.score.player2Wins++;
+    console.log(game.score);
+    $('.currentScore2').text(game.score.player2Wins);
+
+  } else{
+    console.log('p2 did not match. no points earned');
+  }
+  //start of win check in roundScore
+if( (game.score.player1Wins === 3) && (game.score.player2Wins === 3) ){
+  console.log('The game is over! You tied! The game will start over now.');
+  startGame();
+} else if( game.score.player1Wins === 3 ){
+  console.log('Player 1 has won! Click the start button to play again.');
+} else if( game.score.player2Wins === 3 ){
+  console.log('Player 2 has won! Click the start button to play again');
+//game victory check. this will need to scale when player number is editable.
+} else {
+  console.log('victory not yet achieved.');
 }
-roundScore();
+  //end of win check in roundScore
+}
+//roundScore();
+
+
 
 function resultFieldUpdate (){
   $('.resultField').css('background-color','orange');
@@ -87,14 +128,13 @@ function resultFieldUpdate (){
 //$('button').eq(0) returns a jquery object with 1 element. use this to make listeners. below may be wonky, improve.
 function delay(){
   $('.resultField').css('background-color','grey'),700;
-
-  console.log('background should be grey');
-}
+} //flashes the result background-color to indicate new result/successful roll
 
 function startGame() {
   console.log(game.score);
   game.score.player1Wins=0;
   game.score.player2Wins=0;
+  console.log('needs to update score text fields');
   console.log('scores set to 0, callback');
 }
 
