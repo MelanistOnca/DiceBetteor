7f105ace552a2dbb10a72bee31641378cc0c6f3a
Patrick M <Patrick.M.Professional@gmail.com>
2016-02-06 21:40:08 -0500

    going to call it on trying to get N players to work for now, and work on prettying up the CSS for presentation on Monday.

--- js/scripts.js
+++ js/scripts.js
@@ -133,7 +133,7 @@ for(var i=0;i<game.playerCount;i++){
 //start of win check
 
 function winCheck(){
-  consol.log('winCheck was called.');
+  console.log('winCheck was called.');
   if( (game.score.player1Wins >= 3) && (game.score.player2Wins >= 3) ){
   console.log('The game is over! You tied! The game will start over now.');
   alert('The game is over! You tied! The game will start over now.');
@@ -179,26 +179,73 @@ function diceSizer(){
   console.log('diceSizer was called.');
   dice = new Dice($('.diceChooser')[0].value);
   //needs to change .p1Chooser and .p2Chooser to allow for full dice size selection. added playerSideChooser to see if i can just use that.
-  adjustChoosers() ;
+   ;
 }
 
+
+/// adjustChoosers needs updated for the now function-generated player selection areas. numbers seem to be generating outside dropdown menu.
+
+//from similar loop
+
+  // $('label').eq( (i+1) ).attr('for','p'+i+'DiceChoice').text('Pick your result:');//i+1 is because of previous labels- playerNumber and selectDice. //add text to label
+  // //attr('value',i);
+  // console.log('player choice label added for iteration ' +i);
+  // rowDiv.append('<select>');//adds select to div
+  // $('label').eq( (i+1) ).addClass("p"+i+"Chooser playerSideChooser").attr('name','p'+i+'Drop');//i+1 is because of previous selects- playerChooser and diceChooser. //classes select and tries to add name attribute
+
+
 function adjustChoosers(  ) {
+  console.log($('select'));
   console.log('adjustChoosers was called.');
   //var pChoose = $('.playerSideChooser')
+  $('.playerSideChooser').append('<select>');
+  console.log('$(.playerSideChooser).append(<select>); happened - apostrophes removed from this message');
+  console.log($('select'));
   for(var i=0;i<( parseInt(dice.sides)+1 );i++){
-     $('.playerSideChooser').append('<option>');
+//changed roll button CSS tag to "whammy" and this seems to have broken.
+
      if(i===0){
+      //  $('.playerSideChooser').append('<select>');
+      console.log('adjustChoosers if i===0 start');
+      console.log($('select'));
+       $('.playerSideChooser select:first-child').append('<option>');
        $('.playerSideChooser option:first-child').attr('value',i);
-       $('.playerSideChooser option:first-child').text('')
+       $('.playerSideChooser option:first-child').text('');
+       console.log($('select'));
+       console.log('adjustChoosers i===0 end');
      } else{
-     $('.playerSideChooser option:nth-child('+(i+1)+')').attr('value',''+(i));
-     $('.playerSideChooser option:nth-child('+(i+1)+')').text(i);
-     console.log('adjustChoosers loop ' +i + 'th i');
+      //  $('.playerSideChooser').append('<select>');
+      console.log(' i else start');
+      console.log($('select'));
+       $('.playerSideChooser select:first-child').append('<option>');
+       $('.playerSideChooser option:nth-child('+(i+1)+')').attr('value',''+(i));
+       $('.playerSideChooser option:nth-child('+(i+1)+')').text(i);
+       console.log($('select'));
+       console.log('i else end');
+     console.log('adjustChoosers loop ' +i + 'th i');//something in playerNumbers is causing the first 2 blank selects to generate.
    }
-  }//getting double the number of options intended. may be due to 2 elements with class '.playerSideChooser'. may need to loop for something like var c=$('.playerSideChooser').length then loop inside of that? maybe divide the conditional by the .length?
+  }
   //use first-child and last-child?
 }
 
+//begin original adjustChoosers
+    // function adjustChoosers(  ) {
+    //   console.log('adjustChoosers was called.');
+    //   //var pChoose = $('.playerSideChooser')
+    //   for(var i=0;i<( parseInt(dice.sides)+1 );i++){
+    //      $('.playerSideChooser').append('<option>');
+    //      if(i===0){
+    //        $('.playerSideChooser option:first-child').attr('value',i);
+    //        $('.playerSideChooser option:first-child').text('')
+    //      } else{
+    //      $('.playerSideChooser option:nth-child('+(i+1)+')').attr('value',''+(i));
+    //      $('.playerSideChooser option:nth-child('+(i+1)+')').text(i);
+    //      console.log('adjustChoosers loop ' +i + 'th i');
+    //    }
+    //   }
+    //   //use first-child and last-child?
+    // }
+//end original adjustChoosers
 
 
 //changing the number of players will require additional 'player containers'. I should be able to set up a player identifier interface to change labels, and scale to n. then can use these labels to dictate further item generation (chooser/scoreboard). ALSO win conditions will need to be generalized.
@@ -211,51 +258,82 @@ function playerNumbers(){
   console.log('playerNumbers was called.');
   game.playerCount = $('.playerChooser')[0].value;
   //create more player container div elements, including p1dicechoice, p1chooser. also player columns in players score container
-
-//removed code from below for loop// $('.row div:nth-child(3)').attr('test','test')
-// if(i===0){
-//   $('.row div:first-child').attr('value',i);
-//   $('.playerSideChooser option:first-child').text('')
-// } else{
+  var rowDiv;
   function addDiv(){
-    $('.row').append('<div>');//adds div
+    console.log('addDiv called');
+    $('.playersN').append('<div>').addClass('chooser');//adds div
+
+    console.log($('.playersN'));//for testing
     console.log('appended div ');
   }
   function addH2AndClassIt(i){
-    $('.row div:nth-child(' +i+ ')').append('<h2>').addClass("player player"+i+" container one-half column u-max-full-width ");//adds h2 to div and classes h2
+    //var rowDiv =$('.row + div :nth-child(' +i+ ')');
+    $( '.playersN div '/*:nth-child(' + i + ')'*/ ).append('<h2>').addClass("player player"+i+" container one-half column u-max-full-width ");//adds h2 to div and classes h2 //taking out nth child there did SOMETHING.
     console.log('appended h2 and added class names ' +i+ ' times');
+    console.log($( '.playersN + div :nth-child(' + i + ')' ));
+    console.log('row+div :nth-child call');
   }
 
   function putTextInH2(i){
     $('h2').eq( (i-1) ).text('Player '+i);//puts text inside the h2
+    console.log('the following is the element call');
+    $('h2').eq( (i-1) );
+    console.log( $('h2').eq( (i-1) ) );
     console.log('changed the text on h2 ' +i+ ' times');
   }
 
+  // $('label').eq( (i+1) ).attr('for','p'+i+'DiceChoice').text('Pick your result:');//i+1 is because of previous labels- playerNumber and selectDice. //add text to label
+  // //attr('value',i);
+  // console.log('player choice label added for iteration ' +i);
+  // rowDiv.append('<select>');//adds select to div
+  // $('label').eq( (i+1) ).addClass("p"+i+"Chooser playerSideChooser").attr('name','p'+i+'Drop');//i+1 is because of previous selects- playerChooser and diceChooser. //classes select and tries to add name attribute
+
+
+
   function addLabelAndTextItAndAttrIt(i){
-    $('.row div:nth-child(' +i+ ')').append('<label>');//adds label to div
-    $('label').eq( (i+1) ).attr('for','p'+i+'DiceChoice').text('Pick your result:');//i+1 is because of previous labels- playerNumber and selectDice. //add text to label
+    //var rowDiv =$('.row + div :nth-child(' +i+ ')');
+    debugger
+    $('.playersN div :nth-child(' +i+ ')').append('<label>');//adds label to div
+
+    $('label').eq( (i+1) ).attr('for','p'+i+'DiceChoice').text('Pick your result:').attr('value',i);//i+1 is because of previous labels- playerNumber and selectDice. //add text to label
     //attr('value',i);
+    $('.playersN div :nth-child(' +i+ ')').append('<select>');
+    //var rowDiv =$('.row + div :nth-child(' +i+ ')');
+    //may never have appended a label!!
+    $('label').eq( (i+1) ).addClass("p"+i+"Chooser playerSideChooser").attr('name','p'+i+'Drop');
     console.log('player choice label added for iteration ' +i);
-    $('.row div:nth-child(' +i+ ')').append('<select>');//adds select to div
-    $('label').eq( (i+1) ).addClass("p"+i+"Chooser playerSideChooser").attr('name','p'+i+'Drop');//i+1 is because of previous selects- playerChooser and diceChooser. //classes select and tries to add name attribute
+    //i+1 is because of previous selects- playerChooser and diceChooser. //classes select and tries to add name attribute
+    //adds select to div
+    console.log('this is where ths issue is - i think. starting');
+
+    console.log($('label'));
+    console.log('this shows labels after theyve been supposedly added');
+
   }
 
   for(var i=1; i<( parseInt(game.playerCount) +1);i++){
     console.log('playerNumers loop ran.');
     console.log('this is i' + i );
-    console.log('this is game.playerCount ' + game.playerCount);
-    console.log('this is (game.playerCount+1) ' + (game.playerCount+1));
+    // console.log('this is game.playerCount ' + game.playerCount);
+    // console.log('this is (game.playerCount+1) ' +  (parseInt(game.playerCount)+1) );
+
     addDiv(i);
     addH2AndClassIt(i);
     putTextInH2(i);
-    addLabelAndTextItAndAttrIt(i);
-    console.log('post function calls in playerNumbers loop');
+    addLabelAndTextItAndAttrIt(i); //this should be adding the chooser display, but is not.
+
+
+
+
+    // console.log('post function calls in playerNumbers loop');
     //code removed here
 
     // console.log('player chooser select added for iteration' +i);
     // $('.row div:nth-child('+i+')').text(i);
     // console.log('adjustChoosers loop ' +i + 'th i');
     //code removed here
+
+    //think i need to insert chooser format stuff here
 }
 // code removed from above for loop at bottom(just above closing brace)
 // }
@@ -290,12 +368,21 @@ function startGame() {
   $('.playerSideChooser').empty();
   // $('.p1Chooser')[0].value=0;
   // $('.p2Chooser')[0].value=0;
+  // console.log('startGame before diceSizer');
+  // console.log($('select'));
   diceSizer();
+  // console.log($('select'));
+  // console.log('startGame after diceSizer');
   //select dice size. call function.
   //$('.p1Chooser')[0].value
-  console.log('before playerNumbers()');
+  console.log('startGame before playerNumbers');
+  console.log($('select'));
   playerNumbers(); //this is happening 10x for some reason.
-  console.log('after playerNumbers()');
+  console.log($('select'));
+  console.log('startGame after playerNumbers');
+  // console.log('startGame before adjustChoosers called');
+  // console.log($('select'));
+  adjustChoosers();
   //dice = new Dice();
   console.log('scores set to 0, callback');
 }
